16fab2068c398fa9aa9a3b8a8afe720c17c3fce1
juanmabinda <juanmanuelbinda@gmail.com>
2023-12-03 18:38:54 -0300

    Finalizado

--- m04_funciones/src/main.ts
+++ m04_funciones/src/main.ts
@@ -6,37 +6,37 @@ Se agrega una validación para que el número de turno no sea negativo tanto
 cuando se presiona el botón "Anterior" o se define un nuevo turno.
  */
 
-function reset() {
-  const turno = document.getElementById("numero-turno");
+function reset() : void {
+  const turno : HTMLElement | null = document.getElementById("numero-turno");
   if (turno !== null && turno !== undefined) {
     turno.innerHTML = "00";
   };
 };
 
-function restar() {
-  const turno = document.getElementById("numero-turno");
+function restar() : void {
+  const turno : HTMLElement | null = document.getElementById("numero-turno");
   if (turno !== null && turno !== undefined) {
-  let turnoActual = parseInt(turno.innerHTML);
-  const nuevoTurno = turnoActual -= 1;
+  let turnoActual : number = parseInt(turno.innerHTML);
+  const nuevoTurno : number = turnoActual -= 1;
   if (turnoActual >= 0) {
     turno.innerHTML = nuevoTurno.toString().padStart(2, "0");
   };
   };
 };
 
-function sumar() {
-  const turno = document.getElementById("numero-turno");
+function sumar() : void {
+  const turno : HTMLElement | null = document.getElementById("numero-turno");
   if (turno !== null && turno !== undefined) {
-    let turnoActual = parseInt(turno.innerHTML);
-    const nuevoTurno = turnoActual += 1;
+    let turnoActual : number = parseInt(turno.innerHTML);
+    const nuevoTurno : number = turnoActual += 1;
     turno.innerHTML = nuevoTurno.toString().padStart(2, "0");
   }
 
 };
 
-function definirTurno() {
-  const turno = document.getElementById("numero-turno");
-  const nuevoTurno = document.getElementById("cambiar-numero");
+function definirTurno() : void {
+  const turno : HTMLElement | null = document.getElementById("numero-turno");
+  const nuevoTurno : HTMLElement | null = document.getElementById("cambiar-numero");
   if (nuevoTurno !== null && nuevoTurno !== undefined) {
     if(parseInt((nuevoTurno as HTMLInputElement).value) >= 0) {
       if (turno !== null && turno !== undefined) {
@@ -48,22 +48,22 @@ function definirTurno() {
 };
 
 
-const siguiente = document.getElementById("siguiente");
+const siguiente : HTMLElement | null = document.getElementById("siguiente");
 if (siguiente !== null && siguiente !== undefined) {
   siguiente.addEventListener("click", sumar);
 };
 
-const anterior = document.getElementById("anterior");
+const anterior : HTMLElement | null = document.getElementById("anterior");
 if (anterior !== null && anterior !== undefined) {
   anterior.addEventListener("click", restar);
 };
 
-const resetear = document.getElementById("reset");
+const resetear : HTMLElement | null = document.getElementById("reset");
 if (resetear !== null && resetear !== undefined) {
   resetear.addEventListener("click", reset);
 };
 
-const definir = document.getElementById("cambiar-turno");
+const definir : HTMLElement | null = document.getElementById("cambiar-turno");
 if (definir !== null && definir !== undefined) {
   definir.addEventListener("click", definirTurno);
 };
